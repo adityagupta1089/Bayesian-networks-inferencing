534b3abc15e3c46dd33cb7e11cd79c20093c6fd2
Aditya Gupta <adityagupta1089@gmail.com>
2017-04-08 02:12:16 +0530

    .

--- src/queries.cpp
+++ src/queries.cpp
@@ -1,2 +1,5 @@
+#ifndef QUERIES_CPP
+#define QUERIES_CPP
 void process_queries(char* file_name) {
 }
+#endif /*QUERIES_CPP*/
--- src/read_write.cpp
+++ src/read_write.cpp
@@ -1,5 +1,8 @@
+#ifndef READ_WRITE_CPP
+#define READ_WRITE_CPP
 void read_input(char* file_name) {
 }
 
 void write_results(char* file_name) {
 }
+#endif /*READ_WRITE_CPP*/
