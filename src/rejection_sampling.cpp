7adb0d2d8f5aad10cf5673e21dabc713dd162a3b
Aditya Gupta <adityagupta1089@gmail.com>
2017-04-10 15:43:25 +0530

    .

--- src/rejection_sampling.cpp
+++ src/rejection_sampling.cpp
@@ -5,14 +5,18 @@
 
 struct network;
 
-void process_query_rejection_sampling(network& _network, std::vector<int>& query_variables,
-		std::vector<int>& evidence_variables, std::ofstream& out) {
+void process_query_rejection_sampling(network& _network,
+		std::vector<int>& query_variables, std::vector<int>& evidence_variables,
+		std::ofstream& out) {
 	//TODO
 	/* repeat for SAMPLE_COUNT */
+	{
 		/* for each node x_i */
+		{
 			/* sample x_i from cpt */
 			/* if x_i not consistent with evidence go to next cycle*/
 			/* else maintain some count of x_i */
-
+		}
+	}
 	/* calculate probability for given query */
 }
--- src/variable_elimination.cpp
+++ src/variable_elimination.cpp
@@ -3,13 +3,16 @@
 
 struct network;
 
-void process_query_variable_elimination(network& _network, std::vector<int>& query_variables,
-		std::vector<int>& evidence_variables, std::ofstream& out) {
+void process_query_variable_elimination(network& _network,
+		std::vector<int>& query_variables, std::vector<int>& evidence_variables,
+		std::ofstream& out) {
 	//TODO
 	/* Start with initial factors (Local CPTs instantiated by evidence variable) */
 	/* For each hidden variable H */
+	{
 		/* Join all factors with H */
 		/* Eliminate/Sum H */
+	}
 	/* Join all remaining factors */
 	/* Normalize */
 	/* Write to output file*/
